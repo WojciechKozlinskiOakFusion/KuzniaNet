c136329a64cc341918f2cc5fca020ae4a8e22b51
pmajerczyk22 <pr.majerczyk@gmail.com>
2022-04-15 12:18:49 +0200

    First card model

---
+++ SpaceCardGame/SpaceCardGame.Domain/Card_model.cs
@@ -0,0 +1,34 @@
+﻿namespace SpaceCardGame.Domain
+{
+    class Card
+    {
+
+        private string name;
+        private int strenght;
+        private int power;
+        private int DefenseFieldValue;
+        private int DefenseArmor;
+
+        public Card(string name, int strenght, int power, int DefenseFieldValue, int DefenseArmor)
+        {
+            this.name = name;
+            this.strenght = strenght;
+            this.power = power;
+            this.DefenseFieldValue = DefenseFieldValue;
+            this.DefenseArmor = DefenseArmor;
+        }
+        //Id
+        //Name
+        //Power 1
+        //Power 2
+        //Defense Field Value
+        //Defense Armor
+        //DPS - peer Round*?
+    }
+}
+
+
+
+
+
+        
