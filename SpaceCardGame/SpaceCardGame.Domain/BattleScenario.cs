a9d2b88500cbb0222af3ea08fc06e0b9d629270b
sg.golebiowski <sg.golebiowski@gmail.com>
2022-04-17 13:20:59 +0200

    Dodałem BattleScenario

---
+++ SpaceCardGame/SpaceCardGame.Domain/BattleScenario.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace SpaceCardGame.Domain
+{
+    public abstract class BattleScenario
+    {
+        private List<Player> players;
+
+        public void addPlayers(//Lista graczy?, pojedynczy gracze?)
+        {
+             
+        }
+
+        public void selectTypeOfBattle() // typ bitwy, odwołanie do enum OperationType?
+        {
+
+        }
+
+        private virtual bool victoryCondition() // każdy gracz będzie miał właściwość zwycięstwo, przegrana?
+        {
+
+        }
+
+        private virtual bool lossCondition()
+        {
+
+        }
+        
+        private virtual DateTime roundTime() // argument wejściowy string z eum OperationType?
+        {
+
+        }
+
+        private virtual int numberOfRounds() // argument wejściowy string z enum OperationType?
+        {
+
+        }
+
+
+}
+
+}
