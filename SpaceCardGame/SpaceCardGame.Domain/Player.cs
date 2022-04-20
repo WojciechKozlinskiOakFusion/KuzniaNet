8f2e38b8f68b9be64b73dcb18024208d6d0d38fc
Lipsky <mlipin11@gmail.com>
2022-04-20 17:45:17 +0200

    separate get and set

--- SpaceCardGame/SpaceCardGame.Domain/Player.cs
+++ SpaceCardGame/SpaceCardGame.Domain/Player.cs
@@ -8,12 +8,22 @@ namespace SpaceCardGame.Domain
 {
     public class Player
     {
-        public int PlayerId { get; set; }
-        public string Name { get; set; }
-        public List<Card> Deck { get; set; } //
-        public int MatchesCount { get; set; }
-        public int WinCount { get; set; }
-        public int LoseCount { get; set; }
+        public int PlayerId { get; }
+        public string Name { get; }
+        public List<Card> Deck { get; } //
+        public int MatchesCount { get; }
+        public int WinCount { get; }
+        public int LoseCount { get; }
+
+        public Player(int playerId, string name, List<Card> deck, int matchesCount, int winCount, int loseCount)
+        {
+            this.PlayerId = playerId;
+            this.Name = name;
+            this.Deck = deck;
+            this.MatchesCount = matchesCount;
+            this.WinCount = winCount;
+            this.LoseCount = loseCount;
+        }
 
     }
 }
