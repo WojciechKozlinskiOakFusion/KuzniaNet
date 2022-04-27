1e9c5cc997cc2aaeb40de2f71387b9f1a5fff4bf
Mateusz Ogryzek <mateusz.ogryzek@icloud.com>
2022-04-27 17:16:11 +0200

    CreateRoundModel

---
+++ SpaceCardGame/SpaceCardGame.Domain/CreateRoundModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceCardGame.Domain
+{
+    public enum OperationType
+    {
+        Battle,
+        CaptureTheFlag,
+        Elimination,
+        AllForOne
+    }
+
+    public class CreateRoundModel
+    {
+        public List<int> PlayerIDList { get; }
+        public List<int> CardIDList { get; }
+        public OperationType operationType;
+
+        public void SaveGame()
+        {
+            // SaveGame
+        }
+
+        public CreateRoundModel(List<int> PlayerIDList, List<int> CardIDList, OperationType operationType)
+        {
+            this.PlayerIDList = PlayerIDList;
+            this.CardIDList = CardIDList;
+            this.operationType = operationType;
+        }
+    }
+}
